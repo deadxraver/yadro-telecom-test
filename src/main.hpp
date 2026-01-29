36c62ade29104ed6d87e660c0a2142ff05ef0af5
deadxraver <409853@edu.itmo.ru>
2026-01-29 14:19:55 +0300

    enums for event codes

--- src/main.hpp
+++ src/main.hpp
@@ -2,6 +2,17 @@
 
 #define _MAIN_HPP
 
-// TODO: 
+enum InputEvent {
+  I_CLIENT_CAME = 1,
+  I_CLIENT_TOOK_PLACE,
+  I_CLIENT_WAITS,
+  I_CLIENT_LEFT,
+};
+
+enum OutputEvent {
+  O_CLIENT_LEFT = 11,
+  O_CLIENT_TOOK_PLACE,
+  O_ERROR,
+};
 
 #endif // !_MAIN_HPP
